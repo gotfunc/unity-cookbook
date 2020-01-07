e258c93142b4a399322915c865d23fd6d72184aa
Ben Villiere <22590566+gotfunc@users.noreply.github.com>
2020-01-07 17:11:21 +0000

    Create SlerpPosition.cs

---
+++ SlerpPosition.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Vector3 from = new Vector3 (-2.5f, 0f, 0f);
+    public Vector3 to = new Vector3 (2.5f, 0f, 0f);
+    
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, to, 0.75f * Time.deltaTime);
+    }
+}
